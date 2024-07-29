f88e38668eb5da9431ff65815bd93666e8806270
unknown <20bcs2917@cuchd.in>
2024-07-29 16:20:27 +0530

    url update

--- base_url.js
+++ base_url.js
@@ -1,5 +1,5 @@
 export const BASE_URL_VERIFYMOBILE =
-  "https://mahalaxmi-backend.onrender.com/userMaster/verifyMobile";
+  "https://mahalaxmi-backend.onrender.com/verifyMobile";
 
 export const BASE_URL_VERIFYOTP =
-  "https://mahalaxmi-backend.onrender.com/userMaster/verifyOtp";
+  "https://mahalaxmi-backend.onrender.com/verifyOtp";
